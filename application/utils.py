c21ca1a97430d07d54b19c0ea48d76288450ad86
Loaf <acaruanaop@gmail.com>
2021-07-11 21:16:45 +0100

    add utils

---
+++ application/utils.py
@@ -0,0 +1,17 @@
+from application.config import dash_time
+from pynput.keyboard import Controller
+from application.dictionary import chars
+
+def determine_character_press(time):
+    if time >= dash_time:
+        return "-"
+    else:
+        return "."
+    
+def morse_to_character(morse_input):
+    for char in chars:
+        if morse_input == chars[char]:
+            print(f"converted {morse_input} to {char}")
+            return char
+        
+    return False  # only called if no match is found
